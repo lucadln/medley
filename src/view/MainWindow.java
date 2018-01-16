07ced55a687d437fbbc918728b0cf4cb4869a5a3
YONDER\luciana <lucian.ardelean@tss-yonder.com>
2018-01-16 12:45:34 +0200

    TT0: initialize.

---
+++ src/view/MainWindow.java
@@ -0,0 +1,21 @@
+package view;
+
+import javax.swing.*;
+
+public class MainWindow {
+
+    public static void main(String[] args) {
+
+        // Create the frame.
+        JFrame frame = new JFrame("FrameDemo");
+
+        // Optional: What happens when the frame closes?
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Size the frame.
+        frame.pack();
+
+        // Show it.
+        frame.setVisible(true);
+    }
+}
