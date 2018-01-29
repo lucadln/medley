93a591ed989fc4b4e08513c0b84a1f62d3d25d04
YONDER\luciana <lucian.ardelean@tss-yonder.com>
2018-01-29 15:11:39 +0200

    TT: The raw page is now obtained with the entry date and time.

---
+++ timetrack/src/main/java/model/RequestHandler.java
@@ -0,0 +1,100 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+public class RequestHandler {
+
+    public static String executeGet(String targetURL, String cookie) {
+        HttpURLConnection connection = null;
+
+        try {
+            //Create connection
+            URL url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setInstanceFollowRedirects(false);
+
+            connection.setRequestProperty("Cookie", cookie);
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream (
+                    connection.getOutputStream());
+            wr.close();
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
+    public static Map<String, List<String>> executePost(String targetURL, String urlParameters) {
+        HttpURLConnection connection = null;
+
+        try {
+            //Create connection
+            URL url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setInstanceFollowRedirects(false);
+            connection.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded");
+
+            connection.setRequestProperty("Content-Length",
+                    Integer.toString(urlParameters.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream (
+                    connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.close();
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+
+            return connection.getHeaderFields();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+}
--- timetrack/src/main/java/view/MainWindow.java
+++ timetrack/src/main/java/view/MainWindow.java
@@ -1,21 +1,77 @@
 package view;
 
-import javax.swing.*;
+import model.RequestHandler;
+
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+import javax.swing.SwingConstants;
+import java.util.*;
+import java.text.*;
 
 public class MainWindow {
 
-    public static void main(String[] args) {
+    public static void main(String[] arguments) {
 
-        // Create the frame
-        JFrame frame = new JFrame("FrameDemo");
+        String cookie = "";
+        String response;
 
-        // Optional: What happens when the frame closes?
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Login and open the membership page
+        RequestHandler requestHandler = new RequestHandler();
+        Map<String, List<String>> map = requestHandler.executePost(
+                "http://nra2008:80/nroffice/Pages/login.aspx",
+                "__VIEWSTATE=%2FwEPDwUJOTM4MDI3NzQ1D2QWAmYPZBYCAgMPZBYCAgMPZBYCZg9kFgICAQ88KwAJAgAPFgYeDU5ldmVyRXhwYW5kZWRkHgxTZWxlY3RlZE5vZGVkHglMYXN0SW5kZXgCBGQIFCsABWQUKwACFgIeCEV4cGFuZGVkZ2QUKwACFgIfA2dkFCsAAhYCHwNnZBQrAAIWAh8DZ2RkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYCBRljdGwwMCRMb2dpblZpZXckVHJlZVZpZXcxBRxjdGwwMCRDb250ZW50Qm9keSRjaGtSZW1lYmVyDUma3ZqKUmv9AtlbdTeEKbbr65E%3D&__VIEWSTATEGENERATOR=7EEC1B53&__EVENTVALIDATION=%2FwEWBgLKmJi3DgLl48WmDQL37YefDwKXr7zZBgKWo9AlAtjm5rIJ33lWd1AnvfwEVb%2FUaqVsk2Dn6lQ%3D&ctl00%24ContentBody%24LoginUserBox=LUA&ctl00%24ContentBody%24LoginPasswordBox=&ctl00%24ContentBody%24LoginButton=Autentifica-ma");
 
-        // Size the frame
-        frame.pack();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            if (entry.getKey() != null) {
+                if (entry.getKey().toString().equals("Set-Cookie")) {
+                    cookie = entry.getValue().toString().replace("path=/; HttpOnly, ", "").replace("; path=/; HttpOnly", "")
+                                .replace("[", "").replace("]", "");
+                    System.out.println(cookie);
+                }
+            }
+        }
 
-        // Show frame
-        frame.setVisible(true);
+        response = requestHandler.executeGet("http://nra2008:80/nroffice/Pages/Membership/query.aspx", cookie);
+
+        ClockLabel timeLable = new ClockLabel();
+
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame f = new JFrame("Digital Clock");
+        f.setSize(300,150);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLayout(new GridLayout(1, 1));
+
+        f.add(timeLable);
+
+        f.getContentPane().setBackground(Color.black);
+
+        f.setVisible(true);
+    }
+}
+
+class ClockLabel extends JLabel implements ActionListener {
+
+    SimpleDateFormat sdf;
+
+    public ClockLabel() {
+        setForeground(Color.green);
+
+        sdf = new SimpleDateFormat("HH:mm:ss");
+        setFont(new Font("sans-serif", Font.PLAIN, 40));
+        setHorizontalAlignment(SwingConstants.CENTER);
+
+        Timer t = new Timer(1000, this);
+        t.start();
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        Date d = new Date();
+        setText(sdf.format(d));
     }
 }
